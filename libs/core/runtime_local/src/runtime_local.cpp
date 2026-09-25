5f445f115cc056cc58cc2fbafddfa75dfe3b1f12
v4xsh <vanshdobhal11@gmail.com>
2026-09-26 01:31:12 +0530

    runtime: pass complete_version to tracing_init
    Signed-off-by: v4xsh <vanshdobhal11@gmail.com>

--- libs/core/runtime_local/src/runtime_local.cpp
+++ libs/core/runtime_local/src/runtime_local.cpp
@@ -1512,16 +1512,11 @@ namespace hpx {
         _isatty(0);
 #endif
 
-        // initialize instrumentation system
-        hpx::tracing::tracing_init(nullptr, 0, nullptr);
-
-        // record build metadata so a capture can be correlated with
-        // the HPX build that produced it
-        {
-            auto const version_info = hpx::complete_version();
-            hpx::tracing::emit_appinfo(
-                version_info.c_str(), version_info.size());
-        }
+        // initialize instrumentation system; pass build metadata so a
+        // capture can be correlated with the HPX build that produced it
+        auto const version_info = hpx::complete_version();
+        hpx::tracing::tracing_init(
+            nullptr, 0, nullptr, 0, 1, version_info);
 
         LRT_(info).format("cmd_line: {}", get_config().get_cmd_line());
 
